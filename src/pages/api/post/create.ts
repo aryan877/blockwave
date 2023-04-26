687225a6bed73992e233aa9b92c0438a23956a98
Aryan <38075578+aryan877@users.noreply.github.com>
2023-04-27 00:41:19 +0530

    updation of tmp

--- src/pages/api/post/create.ts
+++ src/pages/api/post/create.ts
@@ -2,10 +2,8 @@ import { ProtocolEnum, SpheronClient } from '@spheron/storage';
 // import { rejects } from 'assert';
 // import { verifyMessage } from 'ethers/lib/utils';
 import formidable from 'formidable';
-import fs from 'fs/promises';
 import { withIronSessionApiRoute } from 'iron-session/next';
 import { NextApiRequest, NextApiResponse } from 'next';
-import path from 'path';
 import client from '../../../../lib/sanityBackendClient';
 import { ironOptions } from '../../../../utils';
 const sphToken = process.env.SPHERON_TOKEN as string;
@@ -33,7 +31,7 @@ const readForm = (
 ): Promise<{ fields: formidable.Fields; files: any }> => {
   const options: formidable.Options = {};
   if (saveLocally) {
-    options.uploadDir = path.join(process.cwd(), '/public/images');
+    options.uploadDir = '/tmp';
     options.filename = (name, ext, path, form) => {
       return Date.now().toString() + '_' + path.originalFilename;
     };
@@ -50,23 +48,15 @@ const readForm = (
 
 const createPost = async (req: NextApiRequest, res: NextApiResponse) => {
   //verify wallet signature here, only if signature is valid then the user can post
-
   if (!req.session.siwe?.address) {
     return res.status(422).json({ message: 'Invalid token' });
   }
-  try {
-    await fs.readdir(path.join(process.cwd() + '/public', '/images'));
-  } catch (error) {
-    await fs.mkdir(path.join(process.cwd() + '/public', '/images'));
-  }
-  let _files;
   try {
     if (req.method !== 'POST') {
       res.status(405).json({ message: 'Method not allowed' });
       return;
     }
     const { fields, files } = await readForm(req, true);
-    _files = files;
     if (!fields.txt && !files.image) {
       if (req.method !== 'POST') {
         res.status(405).json({
@@ -75,9 +65,8 @@ const createPost = async (req: NextApiRequest, res: NextApiResponse) => {
         return;
       }
     }
-    let postImage = ''; // initialize postImage to empty string
+    let postImage = '';
     if (files.image) {
-      // check if an image was uploaded
       const { uploadId, bucketId, protocolLink, dynamicLinks } =
         await sphClient.upload(`${files.image.filepath}`, {
           protocol: ProtocolEnum.IPFS,
@@ -87,7 +76,6 @@ const createPost = async (req: NextApiRequest, res: NextApiResponse) => {
             currentlyUploaded += uploadedSize;
           },
         });
-      await fs.unlink(files.image.filepath);
       postImage = `${protocolLink}/${files.image.newFilename}`; // set postImage to the uploaded image link
     }
 
@@ -112,13 +100,8 @@ const createPost = async (req: NextApiRequest, res: NextApiResponse) => {
       ...result,
       author: authorDoc,
     };
-
-    // Update the post document with the populated author reference
     res.json({ postWithAuthor });
   } catch (error) {
-    if (_files && _files.image && _files.image.filepath) {
-      await fs.unlink(_files.image.filepath);
-    }
     console.log(error);
     res.status(500).json({ message: 'Internal server error' });
   }
